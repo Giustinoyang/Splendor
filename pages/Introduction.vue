956f3ee3140b3c105a2a1861242b1fdb81773aac
Giustinoyang <giustinoyang@gmail.com>
2021-05-10 14:53:44 +0200

    Initial commit

---
+++ pages/Introduction.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="splash-container">
+        <div class="splash">
+            <h1 class="splash-head">SPLENDOR</h1>
+            <p>
+                the introduction of Splendor
+            </p>
+        </div>
+        
+    </div>
+</template>
+<script>
+    export default {}
+</script>
