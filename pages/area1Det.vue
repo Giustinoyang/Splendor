13b7ef44aa23f9f5006ca2884b405da36d58efd5
Giustinoyang <giustinoyang@gmail.com>
2021-05-12 15:32:08 +0200

    5/11

---
+++ pages/area1Det.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="splash-container">
+        <div class="cotologe">
+            <a href="areaList">Area List</a> -> Area1
+        </div>
+        <div>
+            <div class="splash">
+                <h1 class="splash-head">AREA1</h1>
+            </div>   
+        </div>
+        <dev class="transLink">
+            <div class="pure-g">
+                <div class="pure-u-1-3"><a href="area1Over">Overview</a></div>
+                <div class="pure-u-1-3"><a href="area1Det"><i>Details</i></a></div>
+                <div class="pure-u-1-3"><a href="area1More">More about</a></div>
+            </div>
+        </dev>
+        <div class="maintext">
+            <p>Details</p>
+            More picture and more Words.
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style scoped>
+.cotologe{
+    width: 80%;
+    height: 3%;
+    margin: auto;
+    position: absolute;
+    top: -85%; left: 0; bottom: 0; right: 0;
+    line-height: 3em;
+    color: white;
+}
+.transLink{
+    width: 80%;
+    height: 5%;
+    margin: auto;
+    position: absolute;
+    top: -60%; left: 0; bottom: 0; right: 0;
+    text-align: center;
+    line-height: 3em;
+    border: 3px solid white;
+    color: white;
+}
+
+.maintext{
+    width: 80%;
+    height: 50%;
+    margin: auto;
+    position: absolute;
+    top: 0; left: 0; bottom: 0; right: 0;
+    text-align: center;
+    line-height: 3em;
+    border: 3px solid white;
+    color: white;
+    font-size: 20px;
+    font-weight: bold;
+}
+
+a:link, a:active{
+    text-decoration: none;
+    color: white;
+}
+</style>
---
+++ pages/areaList.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="splash-container">
+
+        <div class="splash">
+            <h1 class="splash-head">AREA LIST</h1>
+        </div>   
+  
+        <div class="list_area">
+            <div class="list_head">
+                <div class="area_head">
+                    Area1
+                </div>
+                <div class="area_head">
+                    Area2
+                </div>
+            </div>
+            <div class="list_text">
+                <div class="area_text">
+                    This Area is...
+                    <a href="area1Over">more detail</a>
+                </div>
+                <div class="area_text">
+                    This Area is...
+                    <a href="area2">more detail</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style scoped>
+.list_area{
+    width: 80%;
+    height: 50%;
+    margin: auto;
+    position: absolute;
+    top: 0%; left: 0%; bottom: 0%; right: 0%;
+    text-align: center;
+    line-height: 5em;
+}
+.list_head{
+    width: 30%;
+    margin: auto;
+    position: absolute;
+    text-align: center;
+}
+
+.list_text{
+    width: 60%;
+    margin: auto;
+    left: 40%;
+    position: absolute;
+    text-align: center;
+}
+
+.area_head{
+    border: 3px solid white;
+    border-radius: 50px;
+    margin-bottom: 1em;
+}
+
+.area_text{
+    border: 3px solid white;
+    border-radius: 5px;
+    margin-bottom: 1em;
+}
+</style>
